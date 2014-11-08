18c005332c405fa571da0a860985543f8894ecda
Anthony Candaele <anthony.candaele@webmove.be>
2014-11-08 13:36:25 +0100

    reset carrierwave settings to store images locally in development mode

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,14 +7,29 @@ CarrierWave.configure do |config|
       :aws_secret_access_key  => Rails.application.secrets.S3_SECRET,                        # required
     }
     config.fog_directory  = 'beautysalon2'                     # required
+  elsif Rails.env.test?
+    config.storage = :file
+    config.enable_processing = false
+
+    # make sure our uploader is auto-loaded
+    PromotionImageUploader
+
+    # use different dirs when testing
+    CarrierWave::Uploader::Base.descendants.each do |klass|
+      next if klass.anonymous?
+      klass.class_eval do
+        def cache_dir
+          "#{Rails.root}/spec/support/uploads/tmp"
+        end
+
+        def store_dir
+          "#{Rails.root}/spec/support/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+        end
+      end
+    end
   else
-    config.storage = :fog
-    config.fog_credentials = {
-      :provider               => 'AWS',                        # required
-      :aws_access_key_id      => Rails.application.secrets.S3_KEY,                        # required
-      :aws_secret_access_key  => Rails.application.secrets.S3_SECRET,                        # required
-    }
-    config.fog_directory  = 'posplus'                     # required
+    config.storage = :file
+    config.enable_processing = Rails.env.production?
   end
 
 end
