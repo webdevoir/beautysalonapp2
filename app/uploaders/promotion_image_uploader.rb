9a3950914cda2e76b5299033bcea04d638dd4bca
Anthony Candaele <anthony.candaele@webmove.be>
2014-11-08 11:20:31 +0100

    comment scale method in image uploader

--- app/uploaders/promotion_image_uploader.rb
+++ app/uploaders/promotion_image_uploader.rb
@@ -24,15 +24,15 @@ class PromotionImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :scale => [250, 193]
+  # process :scale => [250, 193]
   #
-  def scale(width, height)
-    # do something
-  end
+  # def scale(width, height)
+  #   # do something
+  # end
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  #   process :resize_to_fit => [50, 50]
+    process :resize_to_fit => [250, 193]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
