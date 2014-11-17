13d433576095b23157201dd9a0c1757c1c8bf1d0
Anthony Candaele <anthony.candaele@webmove.be>
2014-11-17 20:54:54 +0100

    implement maximum length section description

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,7 @@
 class Section < ActiveRecord::Base
   belongs_to :category
   validates :title, :description, presence: true
+  validates :description, length: { maximum: 150, :too_long => "De beschrijving mag maximum 150 karakters lang zijn" }
 
   mount_uploader :image, SectionImageUploader
 end
