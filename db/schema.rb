698c63f846977b3f06f33ff8515286f83ae30d55
Anthony Candaele <anthony.candaele@webmove.be>
2014-11-18 20:33:48 +0100

    fix issue with test database (no position field for sections)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141112171803) do
+ActiveRecord::Schema.define(version: 20141118174355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20141112171803) do
     t.text    "description"
     t.string  "image"
     t.integer "category_id"
+    t.integer "position"
   end
 
   create_table "treatments", force: true do |t|
