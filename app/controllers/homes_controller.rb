a05ccdf190b728e532cb1738855406a9f7f98460
Anthony Candaele <anthony.candaele@webmove.be>
2014-12-01 20:03:51 +0100

    add fragment caching to homepage

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,8 +2,12 @@ class HomesController < ApplicationController
 
   def index
     @promotions = Promotion.all.where(visible: true)
+    fresh_when(@promotions)
     @behandelingen = Treatment.all
+    fresh_when(@behandelingen)
     @products = Product.all
+    fresh_when(@products)
     @sections = Section.all.order(:position)
+    fresh_when(@sections)
   end
 end
