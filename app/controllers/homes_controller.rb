08772be1d88fa63ccb9bc976abebefa361a45da2
Anthony Candaele <anthony.candaele@webmove.be>
2015-02-12 19:20:58 +0100

    order products by position on homepage

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,7 +3,7 @@ class HomesController < ApplicationController
   def index
     @promotions = Promotion.all.where(visible: true)
     @behandelingen = Treatment.all.order(:position)
-    @products = Product.all
+    @products = Product.all.order(:position)
     @sections = Section.all.order(:position)
   end
 end
