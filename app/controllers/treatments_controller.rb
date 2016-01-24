c1554b976aa79e8a224d44d298df16ed4f3578b3
Anthony Candaele <anthony.candaele@webmove.be>
2016-01-24 12:45:03 +0100

    replace global instance variables from partials

--- app/controllers/treatments_controller.rb
+++ app/controllers/treatments_controller.rb
@@ -1,7 +1,9 @@
 class TreatmentsController < ApplicationController
   def index
     @category = Category.find_by_name(params[:category])
-    @treatments = Treatment.sort_by_category(@category.id).where(visible: true)
+    if @category
+      @treatments = Treatment.sort_by_category(@category.id).where(visible: true)
+    end
   end
 
   def show
