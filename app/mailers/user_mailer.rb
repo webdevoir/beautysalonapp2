f29bd88f75182eb833339ce8340acd06ec440976
acandael <info@anthonycandaele.com>
2016-04-21 18:20:14 +0200

    remove mail functionality

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default from: "info@anniek-lambrecht.be"
-
-  def contact_email(contact)
-    @contact = contact
-    mail(to: Rails.application.secrets.owner_email, from: "info@anniek-lambrecht.be", :subject => "Website Contact")
-  end
-end
